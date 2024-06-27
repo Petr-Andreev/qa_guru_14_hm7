595f3b6f132493fec874ba45a0ffc45276789c15
Petr Andreev <drakula2805@gmail.com>
2024-06-27 15:36:56 +0300

    4_commit

---
+++ script_os.py
@@ -0,0 +1,6 @@
+import os
+
+CURRENT_FILE = os.path.abspath(__file__) # получаем абсолютный путь к текущему файлу
+CURRENT_DIRECTORY = os.path.dirname(CURRENT_FILE  ) # получаем абсолютный путь к текущей директории где находится файл с которым работаем
+FILES_DIR = os.path.join(CURRENT_DIRECTORY, 'files') # делаем склейку пути к текущей директории и папки files
+FOLDER_DIR = os.path.join(CURRENT_DIRECTORY, 'my_folder') # делаем склейку пути к текущей директории и папки files
